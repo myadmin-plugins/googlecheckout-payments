123b0036eead21fa89f242ad15177596153db80d
Joe Huss <detain@interserver.net>
2017-07-30 16:54:50 -0400

    various improvements

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,7 +12,7 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 class Plugin {
 
 	public static $name = 'Googlecheckout Plugin';
-	public static $description = 'Allows handling of Googlecheckout emails and honeypots';
+	public static $description = 'Allows handling of Googlecheckout based Payments through their Payment Processor/Payment System.';
 	public static $help = '';
 	public static $type = 'plugin';
 
