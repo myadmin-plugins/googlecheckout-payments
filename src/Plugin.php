cba6d18acd8b5317ec5e639d61c1af50f941ac01
Joe Huss <detain@interserver.net>
2018-11-13 20:29:59 -0500

    localization updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -44,7 +44,7 @@ class Plugin
 		if ($GLOBALS['tf']->ima == 'admin') {
 			function_requirements('has_acl');
 			if (has_acl('client_billing')) {
-				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', 'Googlecheckout');
+				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', __('Googlecheckout'));
 			}
 		}
 	}
@@ -54,7 +54,10 @@ class Plugin
 	 */
 	public static function getRequirements(GenericEvent $event)
 	{
-		$loader = $event->getSubject();
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
 		$loader->add_page_requirement('view_google_transaction', '/../vendor/detain/myadmin-googlecheckout-payments/src/view_google_transaction.php');
 		$loader->add_page_requirement('view_google_order', '/../vendor/detain/myadmin-googlecheckout-payments/src/view_google_order.php');
 		$loader->add_page_requirement('pay_balance_google', '/../vendor/detain/myadmin-googlecheckout-payments/src/pay_balance_google.php');
@@ -63,14 +66,17 @@ class Plugin
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		$settings = $event->getSubject();
-		$settings->add_radio_setting('Billing', 'Google Checkout', 'google_checkout_enabled', 'Enable Google Checkout', 'Enable Google Checkout', GOOGLE_CHECKOUT_ENABLED, [true, false], ['Enabled', 'Disabled']);
-		$settings->add_dropdown_setting('Billing', 'Google Checkout', 'google_checkout_sandbox', 'Use Sandbox/Test Environment', 'Use Sandbox/Test Environment', GOOGLE_CHECKOUT_SANDBOX, [false, true], ['Live Environment', 'Sandbox Test Environment']);
-		$settings->add_text_setting('Billing', 'Google Checkout', 'google_checkout_merchant_id', 'Live Merchant ID', 'Live Merchant ID', (defined('GOOGLE_CHECKOUT_MERCHANT_ID') ? GOOGLE_CHECKOUT_MERCHANT_ID : ''));
-		$settings->add_text_setting('Billing', 'Google Checkout', 'google_checkout_merchant_key', 'Live Merchant Key', 'Live Merchant Key', (defined('GOOGLE_CHECKOUT_MERCHANT_KEY') ? GOOGLE_CHECKOUT_MERCHANT_KEY : ''));
-		$settings->add_text_setting('Billing', 'Google Checkout', 'google_checkout_sandbox_merchant_id', 'Sandbox Merchant ID', 'Sandbox Merchant ID', (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID : ''));
-		$settings->add_text_setting('Billing', 'Google Checkout', 'google_checkout_sandbox_merchant_key', 'Sandbox Merchant Key', 'Sandbox Merchant Key', (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY : ''));
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+		$settings->add_radio_setting(__('Billing'), __('Google Checkout'), 'google_checkout_enabled', __('Enable Google Checkout'), __('Enable Google Checkout'), GOOGLE_CHECKOUT_ENABLED, [true, false], ['Enabled', 'Disabled']);
+		$settings->add_dropdown_setting(__('Billing'), __('Google Checkout'), 'google_checkout_sandbox', __('Use Sandbox/Test Environment'), __('Use Sandbox/Test Environment'), GOOGLE_CHECKOUT_SANDBOX, [false, true], ['Live Environment', 'Sandbox Test Environment']);
+		$settings->add_text_setting(__('Billing'), __('Google Checkout'), 'google_checkout_merchant_id', __('Live Merchant ID'), __('Live Merchant ID'), (defined('GOOGLE_CHECKOUT_MERCHANT_ID') ? GOOGLE_CHECKOUT_MERCHANT_ID : ''));
+		$settings->add_text_setting(__('Billing'), __('Google Checkout'), 'google_checkout_merchant_key', __('Live Merchant Key'), __('Live Merchant Key'), (defined('GOOGLE_CHECKOUT_MERCHANT_KEY') ? GOOGLE_CHECKOUT_MERCHANT_KEY : ''));
+		$settings->add_text_setting(__('Billing'), __('Google Checkout'), 'google_checkout_sandbox_merchant_id', __('Sandbox Merchant ID'), __('Sandbox Merchant ID'), (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID : ''));
+		$settings->add_text_setting(__('Billing'), __('Google Checkout'), 'google_checkout_sandbox_merchant_key', __('Sandbox Merchant Key'), __('Sandbox Merchant Key'), (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY : ''));
 	}
 }
