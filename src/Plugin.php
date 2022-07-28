c183123f94b60513b56fa05393f01db00e74e047
Joe Huss <detain@interserver.net>
2022-07-28 17:05:00 -0400

    formatting update

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,71 +11,71 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  */
 class Plugin
 {
-	public static $name = 'Googlecheckout Plugin';
-	public static $description = 'Allows handling of Googlecheckout based Payments through their Payment Processor/Payment System.';
-	public static $help = '';
-	public static $type = 'plugin';
+    public static $name = 'Googlecheckout Plugin';
+    public static $description = 'Allows handling of Googlecheckout based Payments through their Payment Processor/Payment System.';
+    public static $help = '';
+    public static $type = 'plugin';
 
-	/**
-	 * Plugin constructor.
-	 */
-	public function __construct()
-	{
-	}
+    /**
+     * Plugin constructor.
+     */
+    public function __construct()
+    {
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function getHooks()
-	{
-		return [
-			'system.settings' => [__CLASS__, 'getSettings'],
-			//'ui.menu' => [__CLASS__, 'getMenu'],
-			'function.requirements' => [__CLASS__, 'getRequirements'],
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function getHooks()
+    {
+        return [
+            'system.settings' => [__CLASS__, 'getSettings'],
+            //'ui.menu' => [__CLASS__, 'getMenu'],
+            'function.requirements' => [__CLASS__, 'getRequirements'],
+        ];
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getMenu(GenericEvent $event)
-	{
-		$menu = $event->getSubject();
-		if ($GLOBALS['tf']->ima == 'admin') {
-			function_requirements('has_acl');
-			if (has_acl('client_billing')) {
-			}
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getMenu(GenericEvent $event)
+    {
+        $menu = $event->getSubject();
+        if ($GLOBALS['tf']->ima == 'admin') {
+            function_requirements('has_acl');
+            if (has_acl('client_billing')) {
+            }
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getRequirements(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Plugins\Loader $this->loader
-		 */
-		$loader = $event->getSubject();
-		$loader->add_page_requirement('view_google_transaction', '/../vendor/detain/myadmin-googlecheckout-payments/src/view_google_transaction.php');
-		$loader->add_page_requirement('view_google_order', '/../vendor/detain/myadmin-googlecheckout-payments/src/view_google_order.php');
-		$loader->add_page_requirement('pay_balance_google', '/../vendor/detain/myadmin-googlecheckout-payments/src/pay_balance_google.php');
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getRequirements(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
+        $loader->add_page_requirement('view_google_transaction', '/../vendor/detain/myadmin-googlecheckout-payments/src/view_google_transaction.php');
+        $loader->add_page_requirement('view_google_order', '/../vendor/detain/myadmin-googlecheckout-payments/src/view_google_order.php');
+        $loader->add_page_requirement('pay_balance_google', '/../vendor/detain/myadmin-googlecheckout-payments/src/pay_balance_google.php');
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Settings $settings
-		 **/
-		$settings = $event->getSubject();
-		$settings->add_radio_setting(_('Billing'), _('Google Checkout'), 'google_checkout_enabled', _('Enable Google Checkout'), _('Enable Google Checkout'), GOOGLE_CHECKOUT_ENABLED, [true, false], ['Enabled', 'Disabled']);
-		$settings->add_dropdown_setting(_('Billing'), _('Google Checkout'), 'google_checkout_sandbox', _('Use Sandbox/Test Environment'), _('Use Sandbox/Test Environment'), GOOGLE_CHECKOUT_SANDBOX, [false, true], ['Live Environment', 'Sandbox Test Environment']);
-		$settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_merchant_id', _('Live Merchant ID'), _('Live Merchant ID'), (defined('GOOGLE_CHECKOUT_MERCHANT_ID') ? GOOGLE_CHECKOUT_MERCHANT_ID : ''));
-		$settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_merchant_key', _('Live Merchant Key'), _('Live Merchant Key'), (defined('GOOGLE_CHECKOUT_MERCHANT_KEY') ? GOOGLE_CHECKOUT_MERCHANT_KEY : ''));
-		$settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_sandbox_merchant_id', _('Sandbox Merchant ID'), _('Sandbox Merchant ID'), (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID : ''));
-		$settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_sandbox_merchant_key', _('Sandbox Merchant Key'), _('Sandbox Merchant Key'), (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY : ''));
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+        $settings->add_radio_setting(_('Billing'), _('Google Checkout'), 'google_checkout_enabled', _('Enable Google Checkout'), _('Enable Google Checkout'), GOOGLE_CHECKOUT_ENABLED, [true, false], ['Enabled', 'Disabled']);
+        $settings->add_dropdown_setting(_('Billing'), _('Google Checkout'), 'google_checkout_sandbox', _('Use Sandbox/Test Environment'), _('Use Sandbox/Test Environment'), GOOGLE_CHECKOUT_SANDBOX, [false, true], ['Live Environment', 'Sandbox Test Environment']);
+        $settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_merchant_id', _('Live Merchant ID'), _('Live Merchant ID'), (defined('GOOGLE_CHECKOUT_MERCHANT_ID') ? GOOGLE_CHECKOUT_MERCHANT_ID : ''));
+        $settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_merchant_key', _('Live Merchant Key'), _('Live Merchant Key'), (defined('GOOGLE_CHECKOUT_MERCHANT_KEY') ? GOOGLE_CHECKOUT_MERCHANT_KEY : ''));
+        $settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_sandbox_merchant_id', _('Sandbox Merchant ID'), _('Sandbox Merchant ID'), (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_ID : ''));
+        $settings->add_text_setting(_('Billing'), _('Google Checkout'), 'google_checkout_sandbox_merchant_key', _('Sandbox Merchant Key'), _('Sandbox Merchant Key'), (defined('GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY') ? GOOGLE_CHECKOUT_SANDBOX_MERCHANT_KEY : ''));
+    }
 }
--- src/view_google_transaction.php
+++ src/view_google_transaction.php
@@ -1,67 +1,67 @@
 <?php
 
-	function view_google_transaction()
-	{
-		if ($GLOBALS['tf']->ima == 'admin') {
-			$db = clone $GLOBALS['tf']->db;
-			$module = get_module_name((isset($GLOBALS['tf']->variables->request['module']) ? $GLOBALS['tf']->variables->request['module'] : 'default'));
-			$transaction = $db->real_escape($GLOBALS['tf']->variables->request['transaction']);
-			$db->query("select * from gcheckout where google_order='{$transaction}'");
-			if ($db->num_rows() == 0) {
-				$db = get_module_db($module);
-				$db->query("select * from gcheckout where google_order='{$transaction}'");
-			}
-			if ($db->num_rows() > 0) {
+    function view_google_transaction()
+    {
+        if ($GLOBALS['tf']->ima == 'admin') {
+            $db = clone $GLOBALS['tf']->db;
+            $module = get_module_name((isset($GLOBALS['tf']->variables->request['module']) ? $GLOBALS['tf']->variables->request['module'] : 'default'));
+            $transaction = $db->real_escape($GLOBALS['tf']->variables->request['transaction']);
+            $db->query("select * from gcheckout where google_order='{$transaction}'");
+            if ($db->num_rows() == 0) {
+                $db = get_module_db($module);
+                $db->query("select * from gcheckout where google_order='{$transaction}'");
+            }
+            if ($db->num_rows() > 0) {
 
-				/**
-				 * @param $table
-				 * @param $data
-				 * @return bool|string|\TFTable
-				 * @throws \Exception
-				 * @throws \SmartyException
-				 */
-				function google_table($table, $data)
-				{
-					//echo '<pre>';print_r($data);echo '</pre>';
-					if (is_bool($table) && $table === false) {
-						$started = true;
-						$table = new TFTable;
-						$table->hide_title();
-					} else {
-						$started = false;
-					}
-					foreach ($data as $key => $value) {
-						if (null !== $value && (is_array($value) || trim($value) != '')) {
-							$table->set_col_options('style="vertical-align: top;"');
-							$table->add_field(ucwords(str_replace('_', ' ', $key)), 'r');
-							if (is_array($value)) {
-								$table->add_field(google_table(false, $value));
-							} elseif (preg_match('/^a:[0-9]+:{.*}$/s', $value)) {
-								$table->add_field(google_table(false, myadmin_unstringify($value)));
-							} elseif ($key == 'lid') {
-								$table->add_field($table->make_link('choice=none.search&amp;search='.$value, $value), 'r');
-							} else {
-								$table->add_field($value, 'r');
-							}
-							$table->add_row();
-						}
-					}
-					if ($started === true) {
-						return $table->get_table();
-					} else {
-						return $table;
-					}
-				}
+                /**
+                 * @param $table
+                 * @param $data
+                 * @return bool|string|\TFTable
+                 * @throws \Exception
+                 * @throws \SmartyException
+                 */
+                function google_table($table, $data)
+                {
+                    //echo '<pre>';print_r($data);echo '</pre>';
+                    if (is_bool($table) && $table === false) {
+                        $started = true;
+                        $table = new TFTable();
+                        $table->hide_title();
+                    } else {
+                        $started = false;
+                    }
+                    foreach ($data as $key => $value) {
+                        if (null !== $value && (is_array($value) || trim($value) != '')) {
+                            $table->set_col_options('style="vertical-align: top;"');
+                            $table->add_field(ucwords(str_replace('_', ' ', $key)), 'r');
+                            if (is_array($value)) {
+                                $table->add_field(google_table(false, $value));
+                            } elseif (preg_match('/^a:[0-9]+:{.*}$/s', $value)) {
+                                $table->add_field(google_table(false, myadmin_unstringify($value)));
+                            } elseif ($key == 'lid') {
+                                $table->add_field($table->make_link('choice=none.search&amp;search='.$value, $value), 'r');
+                            } else {
+                                $table->add_field($value, 'r');
+                            }
+                            $table->add_row();
+                        }
+                    }
+                    if ($started === true) {
+                        return $table->get_table();
+                    } else {
+                        return $table;
+                    }
+                }
 
-				while ($db->next_record(MYSQL_ASSOC)) {
-					$table = new TFTable;
-					$table->set_title('Transaction Information');
-					$table = google_table($table, $db->Record);
-					//var_dump($table);exit;
-					add_output($table->get_table());
-				}
-			}
-		} else {
-			add_output('This functionality is for administrators only');
-		}
-	}
+                while ($db->next_record(MYSQL_ASSOC)) {
+                    $table = new TFTable();
+                    $table->set_title('Transaction Information');
+                    $table = google_table($table, $db->Record);
+                    //var_dump($table);exit;
+                    add_output($table->get_table());
+                }
+            }
+        } else {
+            add_output('This functionality is for administrators only');
+        }
+    }
